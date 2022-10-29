0af0e8105b934a5be0a563b4196f59203c77aa40
XHS19 <xhassanx19@gmail.com>
2022-10-29 19:45:46 +0300

    num 1

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function BMI (weigth, heigth) {
+   let result = weigth / (heigth * heigth);
+   return result;
+}
+
+console .log(BMI(80, 1.8))
+
+function Status (BodyMass){
+    if (BodyMass < 18.5){
+        return ("لديك نقص في الوزن")
+    }
+    else if (BodyMass >= 18.5 , BodyMass < 25){
+        return ("وزنك صحي")
+    }
+    else if (BodyMass > 25){
+        return ("لديك زيادة في الوزن")
+    }
+}
+
+function calculate(){
+    let weigth = document.getElementById("weight").value;
+    let heigth = document.getElementById("height").value;
+
+    let bmi_value = BMI (weigth, heigth)
+    let desc = Status (bmi_value)
+
+    let box = document.getElementById('result')
+
+    box.innerText = bmi_value +" == " +desc
+}
+
+
 
